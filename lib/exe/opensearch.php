b058cf7986d76d55b54d22f412305dc3f334aa99
Martin Kulich <kulda.zbraslav@gmail.com>
2020-05-23 12:42:34 +0000

    Initial commit

---
+++ lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>Spirit Pony Leadership</ShortName>
+  <Image width="16" height="16" type="image/x-icon">http://spl2010.instruktori.cz/lib/tpl/spl/images/favicon.ico</Image>
+  <Url type="text/html" template="http://spl2010.instruktori.cz/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="http://spl2010.instruktori.cz/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
